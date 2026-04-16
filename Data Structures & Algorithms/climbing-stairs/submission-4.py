aa6f10ebf0a65f4e00e65506f12aadcb3d654887
Mayank Grover <49029679+MayankG514@users.noreply.github.com>
2026-04-15 21:29:32 -0700

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        # def dfs(i):
+        #     if i >= n:
+        #         return i==n
+        #     return dfs(i+1) + dfs(i+2)
+        
+        # return dfs(0)
+
+        # time complexity -> O(2**n)
+
+        if n<3:
+            return n
+        
+        dp = [0] * (n+1)
+
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3,n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+        
+        return dp[n]
+
+        
