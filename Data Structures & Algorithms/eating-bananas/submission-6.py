61f85b29c2e8ff84eec981fbcf9d45ae48f24f5f
Mayank Grover <49029679+MayankG514@users.noreply.github.com>
2026-04-18 17:41:21 -0700

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low,high = 1,max(piles)
+        ans = high
+
+        while low<=high:
+            m = low + ((high-low)//2)
+            t = 0
+            for p in piles:
+                t+= math.ceil(float(p)/m)
+            
+            if t>h:
+                low = m+1
+            elif t<=h:
+                ans = m
+                high = m-1
+
+        return ans
+        # time complexity -> O(n*log(max(piles)))
+        speed = 1
+        while True:
+            totalTime = 0
+            for i in range(len(piles)):
+                totalTime+= math.ceil(float(piles[i])/speed)
+            if totalTime<=h:
+                return speed
+            speed+=1
+        return speed
+
